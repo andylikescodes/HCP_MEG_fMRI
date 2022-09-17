022be88df46d2d7cf634434399b59d7e246c592d
andylikescodes <andylikescodes@gmail.com>
2022-09-16 19:48:51 -0700

    download script for aws

---
+++ scripts/download_hcp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+path_to_save="/home/deliang@chapman.edu/Documents/HCP_MEG_fMRI/data"
+
+path_to_aws="s3://hcp-openaccess/HCP_1200/"
+
+echo "Start copying ${path_to_aws} to ${path_to_save}..."
+
+aws s3 cp $path_to_aws $path_to_save --recursive
