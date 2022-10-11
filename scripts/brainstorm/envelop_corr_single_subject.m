7ef0ffe57db09b95857c766da9d6cb1b31c88cba
Dehua Liang <andylikescodes@gmail.com>
2022-10-10 23:20:37 -0700

    added  script for enevelop and granger causality

--- scripts/brainstorm/envelop_corr_single_subject.m
+++ scripts/brainstorm/envelop_corr_single_subject.m
@@ -1,5 +1,24 @@
 function envelop_corr_single_subject(subject_id)
 
+% Start Brainstorm
+cd /home/deliang@chapman.edu/Documents/HCP_MEG_fMRI/external/brainstorm3;
+if ~brainstorm('status')
+   brainstorm server local
+end
+
+% Path to a Brainstorm database (= a folder that contains one or more Brainstorm protocols)
+BrainstormDbDir = '/home/deliang@chapman.edu/Documents/HCP_MEG_fMRI/data/brainstorm_db';
+
+% Load a new uploaded database (sets BrainstormDbDir and load all the protocols it contains)
+db_import(BrainstormDbDir);
+
+% Alternative: Set the Brainstorm DB folder
+% (defines where the new protocols are going to be created, but does not load anything)
+% bst_set('BrainstormDbDir', BrainstormDbDir);
+
+% Get the protocol index of an existing protocol (already loaded previously in Brainstorm)
+ProtocolName = 'preliminary_results';
+iProtocol = bst_get('Protocol', ProtocolName);
 
 % Input files
 sFiles = [];
--- scripts/cluster/submit_jobs.bash
+++ scripts/cluster/submit_jobs.bash
@@ -1,9 +1,9 @@
 script_name="$1"
-base_path="/home/deliang@chapman.edu/Documents/HCP_MEG_fMRI/"
+base_path="/home/deliang@chapman.edu/Documents/HCP_MEG_fMRI"
 
-subject_list_file="${base_path}scripts/n_files.txt"
+subject_list_file="${base_path}/scripts/n_files.txt"
 
-sbatch_script_path="${base_path}scripts/cluster/${script_name}"
+sbatch_script_path="${base_path}/scripts/cluster/${script_name}"
 
 mkdir -p sbatch_script_path
 
@@ -12,18 +12,20 @@ mkdir -p sbatch_script_path
 while IFS= read -r line; do 
     filename="${sbatch_script_path}/${line}.sbatch"
     touch filename
-    "#!bin/env bash" >> filename
-    "#SBATCH --job-name=${line}_${script_name}" >> filename
-    "#SBATCH --output=${sbatch_script_path}/${script_name}.out" >> filename
-    "#SBATCH --error=${sbatch_script_path}/${script_name}.err" >> filename
-    "#SBATCH --partition=defq" >> filename
-    "#SBATCH --nodes=1" >> filename
-    "#SBATCH --cpus-per-task=4" >> filename
-    "#SBATCH --mem-per-cpu=8096" >> filename
-    "#SBATCH --ntasks=1" >> filename
-    "#SBATCH --time=12:00:00" >> filename
-    "module load matlab" >> filename
-    ""
+    echo "#!bin/env bash" >> "$filename"
+    echo "#SBATCH --job-name=${line}_${script_name}" >> "$filename"
+    echo "#SBATCH --output=${sbatch_script_path}/${script_name}.out" >> "$filename"
+    echo "#SBATCH --error=${sbatch_script_path}/${script_name}.err" >> "$filename"
+    echo "#SBATCH --partition=defq" >> "$filename"
+    echo "#SBATCH --nodes=1" >> "$filename"
+    echo "#SBATCH --cpus-per-task=4" >> "$filename"
+    echo "#SBATCH --mem-per-cpu=8096" >> "$filename"
+    echo "#SBATCH --ntasks=1" >> "$filename"
+    echo "#SBATCH --time=12:00:00" >> "$filename"
+    echo "module load matlab" >> "$filename"
+    echo "matlab -nodisplay -nosplash -r \"addpath(genpath('${base_path}/scripts')); ${script_name}(${line})\"" >> "$filename";
+
+    sbatch "$filename"
 
 done < subject_list_file
 
